cbdbef2bac118a43a657be9f03e16ba32b5b50f1
LiKaiLing9 <1415218039@qq.com>
2016-08-15 10:27:24 +0800

    初始导入

---
+++ app/src/main/java/com/likailing/android/highlevelfourlkl1/BaseActivity.java
@@ -0,0 +1,9 @@
+package com.likailing.android.highlevelfourlkl1;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Likailing on 2016/8/15.
+ */
+public class BaseActivity extends AppCompatActivity {
+}
